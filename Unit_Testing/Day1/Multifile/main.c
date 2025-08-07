700c1e93462378eb833adbc21e1ad8772f0f4890
Raveena R V <rvraveena8@gmail.com>
2025-08-07 20:46:23 +0530

    Unit Testing

---
+++ Unit_Testing/Day1/Multifile/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "demo.h"
+
+int main(){
+    int a = 10, b = 20, c, d;
+    c = sum(a,b);
+    d = product(a,b);
+    printf("Sum is:  %d\n",c);
+    printf("Square is: %d\n",d);
+
+    return 0;
+}
